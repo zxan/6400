91347939dbc96c630f66b0151a07cf6c5a4545db
jyin62 <jyin62@gatech.edu>
2023-11-07 20:59:13 -0500

    jy

--- Phase3/backend/server.js
+++ Phase3/backend/server.js
@@ -3,15 +3,18 @@ const mysql = require('mysql');
 const app = express();
 const PORT = 8888;
 
+// Add the express.json() middleware to parse JSON data
+app.use(express.json());
+
 // MySQL Connection
 //each controller is a file under the controllers file
 const carController = require('./controllers/cars.js');
 const vendorController = require('./controllers/vendors.js');
+const reportsController = require('./controllers/reports.js');
 
 app.get('/api/getCriterias', carController.getCriterias);//this is an API endpoint that point to the getCriterias function in carController
 app.get('/api/getSearchVendors', vendorController.getSearchVendors);
-const reportsController = require('./controllers/reports.js');
-
+app.post('/api/addVendor', vendorController.addVendor);
 app.get('/api/getSellerReports', reportsController.getSellerReports);
 app.get('/api/getAverageTime', reportsController.getAverageTime);
 app.get('/api/searchCars',carController.searchCars);
