f9db3fc4fb035ec1d8873fb0c681e972d36d574f
jyin62 <jyin62@gatech.edu>
2023-11-12 18:50:14 -0500

    error handling and text field format on addvendor

--- Phase3/frontend/src/AddPartsOrder.js
+++ Phase3/frontend/src/AddPartsOrder.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import { Card, CardContent, Typography, TextField, Button, Grid, Tabs, Tab } from '@mui/material';
 import axios from 'axios';
 import NavBar from './component/navBar';
 
@@ -38,6 +37,21 @@ function AddPartsOrder() {
   };
 
   const handleAddVendor = () => {
+    // Validate phone number format
+    const phoneNumberRegex = /^\d{3}-\d{3}-\d{4}$/;
+    if (!phoneNumberRegex.test(newVendor.phoneNumber)) {
+      // Display an error message (you can customize this part based on your UI framework)
+      alert('Invalid phone number format. Please use xxx-xxx-xxxx format.');
+      return; // Don't proceed with adding the vendor
+    }
+
+    // Validate postal code format
+    const postalCodeRegex = /^\d{5}$/;
+    if (!postalCodeRegex.test(newVendor.postalCode)) {
+      alert('Invalid postal code format. Please use a 5-digit code.');
+      return; // Don't proceed with adding the vendor
+    }
+  
     // Define headers with 'Content-Type' set to 'application/json'
     const headers = {
       'Content-Type': 'application/json',
@@ -65,21 +79,27 @@ function AddPartsOrder() {
       });
   };
   
+  
 
   return (
     <div>
       <NavBar />
       <div style={{ textAlign: 'center' }}>
         <h1>Search for Vendors</h1>
-        <TextField
-          label="Search Vendors"
-          variant="outlined"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        <Button variant="contained" color="primary" onClick={handleSearch}>
-          Search
-        </Button>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Search Vendors"
+              variant="outlined"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              fullWidth
+            />
+            <Button variant="contained" color="primary" onClick={handleSearch}>
+              Search
+            </Button>
+          </Grid>
+        </Grid>
         {showTable && (
           <TableContainer component={Paper}>
             <Table>
@@ -109,47 +129,55 @@ function AddPartsOrder() {
           </TableContainer>
         )}
         <h1>Add New Vendor</h1>
-        <div>
-          <TextField
-            label="Name"
-            variant="outlined"
-            value={newVendor.name}
-            onChange={(e) => setNewVendor({ ...newVendor, name: e.target.value })}
-          />
-          <TextField
-            label="Phone Number"
-            variant="outlined"
-            value={newVendor.phoneNumber}
-            onChange={(e) => setNewVendor({ ...newVendor, phoneNumber: e.target.value })}
-          />
-          <TextField
-            label="Street"
-            variant="outlined"
-            value={newVendor.street}
-            onChange={(e) => setNewVendor({ ...newVendor, street: e.target.value })}
-          />
-          <TextField
-            label="City"
-            variant="outlined"
-            value={newVendor.city}
-            onChange={(e) => setNewVendor({ ...newVendor, city: e.target.value })}
-          />
-          <TextField
-            label="State"
-            variant="outlined"
-            value={newVendor.state}
-            onChange={(e) => setNewVendor({ ...newVendor, state: e.target.value })}
-          />
-          <TextField
-            label="Postal Code"
-            variant="outlined"
-            value={newVendor.postalCode}
-            onChange={(e) => setNewVendor({ ...newVendor, postalCode: e.target.value })}
-          />
-          <Button variant="contained" color="primary" onClick={handleAddVendor}>
-            Add Vendor
-          </Button>
-        </div>
+        <Grid container justifyContent="center">
+          <Grid item xs={12} sm={6}>
+            <TextField
+              label="Name"
+              variant="outlined"
+              value={newVendor.name}
+              onChange={(e) => setNewVendor({ ...newVendor, name: e.target.value })}
+              fullWidth
+            />
+            <TextField
+              label="Phone Number"
+              variant="outlined"
+              value={newVendor.phoneNumber}
+              onChange={(e) => setNewVendor({ ...newVendor, phoneNumber: e.target.value })}
+              fullWidth
+            />
+            <TextField
+              label="Street"
+              variant="outlined"
+              value={newVendor.street}
+              onChange={(e) => setNewVendor({ ...newVendor, street: e.target.value })}
+              fullWidth
+            />
+            <TextField
+              label="City"
+              variant="outlined"
+              value={newVendor.city}
+              onChange={(e) => setNewVendor({ ...newVendor, city: e.target.value })}
+              fullWidth
+            />
+            <TextField
+              label="State"
+              variant="outlined"
+              value={newVendor.state}
+              onChange={(e) => setNewVendor({ ...newVendor, state: e.target.value })}
+              fullWidth
+            />
+            <TextField
+              label="Postal Code"
+              variant="outlined"
+              value={newVendor.postalCode}
+              onChange={(e) => setNewVendor({ ...newVendor, postalCode: e.target.value })}
+              fullWidth
+            />
+            <Button variant="contained" color="primary" onClick={handleAddVendor}>
+              Add Vendor
+            </Button>
+          </Grid>
+        </Grid>
       </div>
     </div>
   );
