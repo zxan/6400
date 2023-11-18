ef26423380ebc25f799266db78705069809d1c16
Ningxin Li <nli311@gatech.edu>
2023-11-18 15:54:27 -0500

    added user authentication for owner/manager in all reports

--- Phase3/frontend/src/PartsStatistics.js
+++ Phase3/frontend/src/PartsStatistics.js
@@ -1,18 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import NavBar from './component/navBar'; // Make sure to adjust the path if needed
+import NavBar from './component/navBar';
 import axios from 'axios';
+import { CircularProgress } from '@mui/material';
 
 function PartsStatistics() {
   const [partsData, setPartsData] = useState([]);
+  const [isManagerOrOwner, setIsManagerOrOwner] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // You may need to adjust the endpoint based on your backend
+    // Fetch user role information when the component mounts
+    const storedUser = sessionStorage.getItem('user');
+
+    axios.get("/api/isManagerOrOwner", { params: { 'username': storedUser } })
+      .then((response) => {
+        if (response.data === true) {
+          setIsManagerOrOwner(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        // Set loading to false when authorization check is complete
+        setLoading(false);
+      });
+
+    // Fetch parts statistics data
     axios.get('/api/getPartsStatistics')
       .then(response => {
         setPartsData(response.data);
       })
       .catch(error => {
         console.error('Error fetching parts statistics:', error);
+        // Set loading to false if there's an error fetching the data
+        setLoading(false);
       });
   }, []);
 
@@ -35,32 +57,51 @@ function PartsStatistics() {
       marginTop: '5%',
       marginBottom: '20px',
     },
+    notAuthorizedMessage: {
+      textAlign: 'center',
+      color: 'red',
+      marginTop: '20px',
+    },
   };
 
   return (
     <div>
       <NavBar />
       <h1 style={styles.title}>Parts Statistics</h1>
-      <table style={styles.table}>
-        <thead>
-          <tr>
-            <th style={styles.columnHeader}>Vendor Name</th>
-            <th style={styles.columnHeader}>Total Parts Supplied</th>
-            <th style={styles.columnHeader}>Total Parts Quantity</th>
-            <th style={styles.columnHeader}>Total Dollar Amount</th>
-          </tr>
-        </thead>
-        <tbody>
-          {partsData.map((entry) => (
-            <tr key={entry.VendorName}>
-              <td>{entry.VendorName}</td>
-              <td>{entry.TotalPartsSupplied}</td>
-              <td>{entry.TotalPartsQuantity}</td>
-              <td>${entry.TotalDollarAmount}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+      {loading ? (
+        // Show a loading indicator while the data is being fetched
+        <CircularProgress style={{ margin: '20px auto', display: 'block' }} />
+      ) : (
+        <>
+          {isManagerOrOwner ? (
+            <table style={styles.table}>
+              <thead>
+                <tr>
+                  <th style={styles.columnHeader}>Vendor Name</th>
+                  <th style={styles.columnHeader}>Total Parts Supplied</th>
+                  <th style={styles.columnHeader}>Total Parts Quantity</th>
+                  <th style={styles.columnHeader}>Total Dollar Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {partsData.map((entry) => (
+                  <tr key={entry.VendorName}>
+                    <td>{entry.VendorName}</td>
+                    <td>{entry.TotalPartsSupplied}</td>
+                    <td>{entry.TotalPartsQuantity}</td>
+                    <td>${entry.TotalDollarAmount}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div style={styles.notAuthorizedMessage}>
+              <p>You are not authorized to view this data.</p>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
--- Phase3/frontend/src/SummaryReport.js
+++ Phase3/frontend/src/SummaryReport.js
@@ -2,18 +2,40 @@ import React, { useState, useEffect } from 'react';
 import NavBar from './component/navBar';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { CircularProgress } from '@mui/material';
 
 function SummaryReport() {
   const [summaryData, setSummaryData] = useState([]);
+  const [isManagerOrOwner, setIsManagerOrOwner] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Adjust the endpoint based on your backend
+    // Fetch user role information when the component mounts
+    const storedUser = sessionStorage.getItem('user');
+
+    axios.get("/api/isManagerOrOwner", { params: { 'username': storedUser } })
+      .then((response) => {
+        if (response.data === true) {
+          setIsManagerOrOwner(true);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        // Set loading to false when authorization check is complete
+        setLoading(false);
+      });
+
+    // Fetch summary report data
     axios.get('/api/getSummaryReport')
       .then(response => {
         setSummaryData(response.data);
       })
       .catch(error => {
         console.error('Error fetching summary report:', error);
+        // Set loading to false if there's an error fetching the data
+        setLoading(false);
       });
   }, []);
 
@@ -50,42 +72,61 @@ function SummaryReport() {
       color: 'blue',
       cursor: 'pointer',
     },
+    notAuthorizedMessage: {
+      textAlign: 'center',
+      color: 'red',
+      marginTop: '20px',
+    },
   };
 
   return (
     <div>
       <NavBar />
       <h1 style={styles.title}>Monthly/Yearly Summary Report</h1>
-      <table style={styles.table}>
-        <thead>
-          <tr>
-            <th style={styles.columnHeader}>Year</th>
-            <th style={styles.columnHeader}>Month</th>
-            <th style={styles.columnHeader}>Total Vehicles Sold</th>
-            <th style={styles.columnHeader}>Total Sales Income</th>
-            <th style={styles.columnHeader}>Total Net Income</th>
-          </tr>
-        </thead>
-        <tbody>
-          {summaryData.map((entry, index) => (
-            <tr key={`${entry.SaleYear}-${entry.SaleMonth}`} style={index % 2 === 0 ? styles.evenRow : null}>
-                <td style={styles.cell}>
-                <Link to={`/SummaryReportDetail/${entry.SaleYear}/0`} style={styles.link}>
-                    {entry.SaleYear}
-                </Link>
-                </td>
-                <td style={styles.cell}>
-                    <Link to={`/SummaryReportDetail/${entry.SaleYear}/${entry.SaleMonth}`} style={styles.link}>
-                    {getMonthName(entry.SaleMonth)}
-                    </Link>
-                </td>
-              <td style={styles.cell}>{entry.TotalVehiclesSold}</td>
-              <td style={styles.cell}>${formatNumber(entry.TotalSalesIncome)}</td>
-              <td style={styles.cell}>${formatNumber(entry.TotalNetIncome)}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+
+      {loading ? (
+        // Show a loading indicator while the data is being fetched
+        <CircularProgress style={{ margin: '20px auto', display: 'block' }} />
+      ) : (
+        <>
+          {isManagerOrOwner ? (
+            <table style={styles.table}>
+              <thead>
+                <tr>
+                  <th style={styles.columnHeader}>Year</th>
+                  <th style={styles.columnHeader}>Month</th>
+                  <th style={styles.columnHeader}>Total Vehicles Sold</th>
+                  <th style={styles.columnHeader}>Total Sales Income</th>
+                  <th style={styles.columnHeader}>Total Net Income</th>
+                </tr>
+              </thead>
+              <tbody>
+                {summaryData.map((entry, index) => (
+                  <tr key={`${entry.SaleYear}-${entry.SaleMonth}`} style={index % 2 === 0 ? styles.evenRow : null}>
+                    <td style={styles.cell}>
+                      <Link to={`/SummaryReportDetail/${entry.SaleYear}/0`} style={styles.link}>
+                        {entry.SaleYear}
+                      </Link>
+                    </td>
+                    <td style={styles.cell}>
+                      <Link to={`/SummaryReportDetail/${entry.SaleYear}/${entry.SaleMonth}`} style={styles.link}>
+                        {getMonthName(entry.SaleMonth)}
+                      </Link>
+                    </td>
+                    <td style={styles.cell}>{entry.TotalVehiclesSold}</td>
+                    <td style={styles.cell}>${formatNumber(entry.TotalSalesIncome)}</td>
+                    <td style={styles.cell}>${formatNumber(entry.TotalNetIncome)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <div style={styles.notAuthorizedMessage}>
+              <p>You are not authorized to view this data.</p>
+            </div>
+          )}
+        </>
+      )}
     </div>
   );
 }
